3cdf8644beada3b5e78f04e45aaf1def00e05f26
Waleed Shahid <iamwaleedshahid@gmail.com>
2023-07-17 18:57:25 +0500

    Remove Projects and Experience from Nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -30,59 +30,6 @@ export default function Navbar() {
             </Link>
           </div>
 
-          <div className="space-x-8 md:block">
-            <Link href="/projects">
-              <a
-                className={`text-base  ${router.asPath === "/projects"
-                  ? "text-gray-800 font-bold dark:text-gray-400"
-                  : "text-gray-600 dark:text-gray-300 font-normal "
-                  }`}
-              >
-                Projects
-                {router.asPath === "/projects" && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-arrow-down inline-block h-3 w-3"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-                    />
-                  </svg>
-                )}
-              </a>
-            </Link>
-            <Link href="/experience">
-              <a
-                className={`text-base  ${router.asPath === "/experience"
-                  ? "text-gray-800 font-bold dark:text-gray-400"
-                  : "text-gray-600 dark:text-gray-300 font-normal "
-                  }`}
-              >
-                Experience{" "}
-                {router.asPath === "/experience" && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-arrow-down inline-block h-3 w-3"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-                    />
-                  </svg>
-                )}
-              </a>
-            </Link>
-          </div>
-
           <div className="space-x-4 flex flex-row items-center">
             <a
               href={`mailto:${userData.email}`}
@@ -151,16 +98,6 @@ export default function Navbar() {
           <Link href="/thinkific">
             <a className="text-base font-normal text-gray-600 dark:text-gray-300">
               Thinkific
-            </a>
-          </Link>
-          <Link href="/projects">
-            <a className="text-base font-normal text-gray-600 dark:text-gray-300">
-              Projects
-            </a>
-          </Link>
-          <Link href="/experience">
-            <a className="text-base font-normal text-gray-600 dark:text-gray-300">
-              Experience
             </a>
           </Link>
         </div>
