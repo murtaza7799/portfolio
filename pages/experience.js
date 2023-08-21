d5261406ba0c8cf156698bab2077ee4d21ccc9e6
Muhammad Murtaza <murtazasarwar@live.com>
2023-08-22 01:31:21 +0500

    Replace name

--- pages/experience.js
+++ pages/experience.js
@@ -4,7 +4,7 @@ import Experience from "../components/Experience";
 
 export default function experience() {
   return (
-    <ContainerBlock title="Experience - Waleed Shahid">
+    <ContainerBlock title="Experience - Murtaza Sarwar">
       <Experience />
     </ContainerBlock>
   );
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Thinkific from "@components/Thinkific";
 export default function Home() {
   return (
     <ContainerBlock
-      title="Waleed Shahid - Full Stack Developer"
+      title="Murtaza Sarwar - Full Stack Developer"
       description="I've been working with Thinkific for over 3 years now. I've worked with over 50 clients and have built over 100 course websites on Thinkific. I've worked with clients like NAS Academy, TwinRay Academy, theChapter Academy, and many more."
     >
       <Thinkific />
